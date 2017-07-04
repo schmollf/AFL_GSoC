a690c4949556a8a8aee53cc3832f6e5c6358f783
Felix Schmoll <eggi.innovations@gmail.com>
2017-07-04 09:48:11 +0200

    Communicate between AFL and XTF

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -79,6 +79,9 @@
 /* Lots of globals, but mostly for the status UI and other things where it
    really makes no sense to haul them around as function parameters. */
 
+int pipefd_to_xtf[2];
+int pipefd_from_xtf[2];
+char* domain;
 
 EXP_ST u8 *in_dir,                    /* Input directory with test cases  */
           *out_file,                  /* File to fuzz, if any             */
@@ -109,7 +112,7 @@ EXP_ST u8  skip_deterministic,        /* Skip deterministic stages?       */
            not_on_tty,                /* stdout is not a tty              */
            term_too_small,            /* terminal dimensions too small    */
            uses_asan,                 /* Target uses ASAN?                */
-           no_forkserver,             /* Disable forkserver?              */
+           no_forkserver = 1,         /* Disable forkserver?              */
            crash_mode,                /* Crash mode! Yeah!                */
            in_place_resume,           /* Attempt in-place resume?         */
            auto_changed,              /* Auto-generated tokens changed?   */
@@ -2256,203 +2259,32 @@ EXP_ST void init_forkserver(char** argv) {
 /* Execute target application, monitoring for timeouts. Return status
    information. The called program will update trace_bits[]. */
 
+/* command to run AFL: sudo ./afl-fuzz -i ~/testcase_dir -o ~/findings_dir -r 35 /path/of/program */
+
 static u8 run_target(char** argv, u32 timeout) {
 
-  static struct itimerval it;
-  static u32 prev_timed_out = 0;
+   static int it = 0;
 
-  int status = 0;
-  u32 tb4;
+   if(it > 4) { exit(0); } else { ++it; }
 
-  child_timed_out = 0;
+   //MARK: do stuff
+   size_t buf_size = 100;
+   char buffer[buf_size];
 
-  /* After this memset, trace_bits[] are effectively volatile, so we
-     must prevent any earlier operations from venturing into that
-     territory. */
+   printf("Enter run_target\n");
 
-  memset(trace_bits, 0, MAP_SIZE);
-  MEM_BARRIER();
+   /* try to write to child and read something back */
+   dprintf(pipefd_to_xtf[1], "Hello from AFL\n");
 
-  /* If we're running in "dumb" mode, we can't rely on the fork server
-     logic compiled into the target program, so we will just keep calling
-     execve(). There is a bit of code duplication between here and 
-     init_forkserver(), but c'est la vie. */
+   int num = read(pipefd_from_xtf[0], buffer, buf_size);
 
-  if (dumb_mode == 1 || no_forkserver) {
-
-    child_pid = fork();
-
-    if (child_pid < 0) PFATAL("fork() failed");
-
-    if (!child_pid) {
-
-      struct rlimit r;
-
-      if (mem_limit) {
-
-        r.rlim_max = r.rlim_cur = ((rlim_t)mem_limit) << 20;
-
-#ifdef RLIMIT_AS
-
-        setrlimit(RLIMIT_AS, &r); /* Ignore errors */
-
-#else
-
-        setrlimit(RLIMIT_DATA, &r); /* Ignore errors */
-
-#endif /* ^RLIMIT_AS */
-
-      }
-
-      r.rlim_max = r.rlim_cur = 0;
-
-      setrlimit(RLIMIT_CORE, &r); /* Ignore errors */
-
-      /* Isolate the process and configure standard descriptors. If out_file is
-         specified, stdin is /dev/null; otherwise, out_fd is cloned instead. */
-
-      setsid();
-
-      dup2(dev_null_fd, 1);
-      dup2(dev_null_fd, 2);
-
-      if (out_file) {
-
-        dup2(dev_null_fd, 0);
-
-      } else {
-
-        dup2(out_fd, 0);
-        close(out_fd);
-
-      }
-
-      /* On Linux, would be faster to use O_CLOEXEC. Maybe TODO. */
-
-      close(dev_null_fd);
-      close(out_dir_fd);
-      close(dev_urandom_fd);
-      close(fileno(plot_file));
-
-      /* Set sane defaults for ASAN if nothing else specified. */
-
-      setenv("ASAN_OPTIONS", "abort_on_error=1:"
-                             "detect_leaks=0:"
-                             "symbolize=0:"
-                             "allocator_may_return_null=1", 0);
-
-      setenv("MSAN_OPTIONS", "exit_code=" STRINGIFY(MSAN_ERROR) ":"
-                             "symbolize=0:"
-                             "msan_track_origins=0", 0);
-
-      execv(target_path, argv);
-
-      /* Use a distinctive bitmap value to tell the parent about execv()
-         falling through. */
-
-      *(u32*)trace_bits = EXEC_FAIL_SIG;
-      exit(0);
-
-    }
-
-  } else {
-
-    s32 res;
-
-    /* In non-dumb mode, we have the fork server up and running, so simply
-       tell it to have at it, and then read back PID. */
-
-    if ((res = write(fsrv_ctl_fd, &prev_timed_out, 4)) != 4) {
-
-      if (stop_soon) return 0;
-      RPFATAL(res, "Unable to request new process from fork server (OOM?)");
-
-    }
-
-    if ((res = read(fsrv_st_fd, &child_pid, 4)) != 4) {
-
-      if (stop_soon) return 0;
-      RPFATAL(res, "Unable to request new process from fork server (OOM?)");
-
-    }
-
-    if (child_pid <= 0) FATAL("Fork server is misbehaving (OOM?)");
-
-  }
-
-  /* Configure timeout, as requested by user, then wait for child to terminate. */
-
-  it.it_value.tv_sec = (timeout / 1000);
-  it.it_value.tv_usec = (timeout % 1000) * 1000;
-
-  setitimer(ITIMER_REAL, &it, NULL);
-
-  /* The SIGALRM handler simply kills the child_pid and sets child_timed_out. */
-
-  if (dumb_mode == 1 || no_forkserver) {
-
-    if (waitpid(child_pid, &status, 0) <= 0) PFATAL("waitpid() failed");
-
-  } else {
-
-    s32 res;
-
-    if ((res = read(fsrv_st_fd, &status, 4)) != 4) {
-
-      if (stop_soon) return 0;
-      RPFATAL(res, "Unable to communicate with fork server (OOM?)");
-
-    }
-
-  }
-
-  if (!WIFSTOPPED(status)) child_pid = 0;
-
-  it.it_value.tv_sec = 0;
-  it.it_value.tv_usec = 0;
-
-  setitimer(ITIMER_REAL, &it, NULL);
-
-  total_execs++;
-
-  /* Any subsequent operations on trace_bits must not be moved by the
-     compiler below this point. Past this location, trace_bits[] behave
-     very normally and do not have to be treated as volatile. */
-
-  MEM_BARRIER();
-
-  tb4 = *(u32*)trace_bits;
-
-#ifdef __x86_64__
-  classify_counts((u64*)trace_bits);
-#else
-  classify_counts((u32*)trace_bits);
-#endif /* ^__x86_64__ */
-
-  prev_timed_out = child_timed_out;
-
-  /* Report outcome to caller. */
-
-  if (child_timed_out) return FAULT_TMOUT;
-
-  if (WIFSIGNALED(status) && !stop_soon) {
-    kill_signal = WTERMSIG(status);
-    return FAULT_CRASH;
-  }
-
-  /* A somewhat nasty hack for MSAN, which doesn't support abort_on_error and
-     must use a special exit code. */
-
-  if (uses_asan && WEXITSTATUS(status) == MSAN_ERROR) {
-    kill_signal = 0;
-    return FAULT_CRASH;
-  }
-
-  if ((dumb_mode == 1 || no_forkserver) && tb4 == EXEC_FAIL_SIG)
-    return FAULT_ERROR;
-
-  return FAULT_NONE;
-
+   if( num <= 0 ) {
+     printf("Couldn't read from XTF\n");
+     return 0;
+   } else {
+     printf("Read from XTF: %s\n", buffer); 
+     return FAULT_NONE;
+   }
 }
 
 
@@ -7665,6 +7497,68 @@ static void save_cmdline(u32 argc, char** argv) {
 
 }
 
+//MARK:
+/** 
+  * @param domid Point to domid of XTF
+
+  * TODO: error checking, always need sudo
+  */
+static void setup_pipe_and_fork(char *domid) {
+
+  pid_t childpid;
+  int ret;
+
+  if(ret = pipe(pipefd_to_xtf) < 0)
+    goto FAIL;
+ 
+  if(ret = pipe(pipefd_from_xtf) < 0)
+    goto FAIL;
+
+  if((childpid = fork()) == -1) {
+    perror("fork");
+    exit(1);
+  }
+
+  if (childpid == 0) { /* child */
+
+    /* close unneeded pipe ends */
+    close(pipefd_to_xtf[1]);
+    close(pipefd_from_xtf[0]);
+
+    /* stdin */
+    close(0);
+    if( (ret = dup(pipefd_to_xtf[0])) < 0 )
+        goto FAIL;
+
+    /* stdout */
+    close(1);
+    if( (ret = dup(pipefd_from_xtf[1])) < 0 )
+        goto FAIL;
+
+    char path[] = "/usr/local/sbin/xl";
+    /* actually need to use second console */
+    if( execl(path, path, "console", "-i", domid,  NULL) < 0 )
+        goto FAIL_FORK;
+
+  } else { /* parent */
+
+    /* close unneeded pipe ends */
+    close(pipefd_to_xtf[0]);
+    close(pipefd_from_xtf[1]);
+
+  }
+
+  return;
+
+  FAIL:
+    printf("Failed to setup pipes, domain: %s, ret = %d\n", domain, ret);
+    exit(1);
+
+  FAIL_FORK:
+    printf("failed fork\n");
+    exit(1);
+}
+
 
 #ifndef AFL_LIB
 
@@ -7690,7 +7584,7 @@ int main(int argc, char** argv) {
   gettimeofday(&tv, &tz);
   srandom(tv.tv_sec ^ tv.tv_usec ^ getpid());
 
-  while ((opt = getopt(argc, argv, "+i:o:f:m:t:T:dnCB:S:M:x:Q")) > 0)
+  while ((opt = getopt(argc, argv, "+i:o:f:m:t:T:dnCB:S:M:x:Qr:")) > 0)
 
     switch (opt) {
 
@@ -7858,6 +7752,12 @@ int main(int argc, char** argv) {
 
         break;
 
+      case 'r': /* domain id for XTF server */
+
+        domain = optarg;
+        printf("domain set: %s\n", domain);
+        break;
+
       default:
 
         usage(argv[0]);
@@ -7935,7 +7835,7 @@ int main(int argc, char** argv) {
 
   if (!out_file) setup_stdio_file();
 
-  check_binary(argv[optind]);
+  //check_binary(argv[optind]);
 
   start_time = get_cur_time();
 
@@ -7944,7 +7844,7 @@ int main(int argc, char** argv) {
   else
     use_argv = argv + optind;
 
-  perform_dry_run(use_argv);
+//  perform_dry_run(use_argv);
 
   cull_queue();
 
@@ -7965,6 +7865,10 @@ int main(int argc, char** argv) {
     if (stop_soon) goto stop_fuzzing;
   }
 
+  //MARK:
+  setup_pipe_and_fork(domain);
+
+  //TODO: how often is this loop executed?!
   while (1) {
 
     u8 skipped_fuzz;
